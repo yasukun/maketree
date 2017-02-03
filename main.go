7d43c66589def7f02101d250c3367ba5de92d7b0
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2017-02-03 18:02:46 +0900

    gofmt

--- main.go
+++ main.go
@@ -14,15 +14,15 @@ var test = flag.Bool("test", false, "test mode")
 func main() {
 	flag.Parse()
 
-  _, err := os.Stat(*input)
-  if os.IsNotExist(err) {
-    fmt.Fprintf(os.Stderr, "no such file: %v\n", err)
-    os.Exit(1)
-  }
+	_, err := os.Stat(*input)
+	if os.IsNotExist(err) {
+		fmt.Fprintf(os.Stderr, "no such file: %v\n", err)
+		os.Exit(1)
+	}
 
 	text, err := mktree.ScanAndClean(*input)
 	if err != nil {
-    fmt.Fprintf(os.Stderr, "Unknown error: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Unknown error: %v\n", err)
 		os.Exit(2)
 	}
 	mktree.MakeTree(&text, *test)
--- mktree/lib_test.go
+++ mktree/lib_test.go
@@ -7,25 +7,25 @@ import (
 )
 
 // TestCleantree ...
-func TestCleantree(t *testing.T)  {
-  cleantree("    │   ├── 201310_00010_2013-230814.txt.csv")
+func TestCleantree(t *testing.T) {
+	cleantree("    │   ├── 201310_00010_2013-230814.txt.csv")
 }
 
 // TestScanAndClean ...
-func TestScanAndClean(t *testing.T)  {
-  text, err := ScanAndClean("../testdata/tree.txt")
-  if err != nil {
-    t.Error(err)
-  }
-  log.Println(text)
-  MakeTree(&text, false);
+func TestScanAndClean(t *testing.T) {
+	text, err := ScanAndClean("../testdata/tree.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	log.Println(text)
+	MakeTree(&text, false)
 }
 
 // TestConutlspasce ...
-func TestConutlspasce(t *testing.T)  {
-  i := countlspace("        workflow")
-  if i == 0 {
-    t.Error("wrong value")
-  }
-  log.Println(i)
+func TestConutlspasce(t *testing.T) {
+	i := countlspace("        workflow")
+	if i == 0 {
+		t.Error("wrong value")
+	}
+	log.Println(i)
 }
